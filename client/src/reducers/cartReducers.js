0f97022e2a32a79e08afed44bb57aec9e4cc91b9
Elif Kutluğ <elif.kutlugg@hotmail.com>
2023-03-22 13:49:14 +0300

    Food-Order-Sepetten silme işlemi gerçekleştirildi

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -5,6 +5,15 @@ export const addToCartReducer = (state = { cartItems: [] }, action) => {
         ...state,
         cartItems: [...state.cartItems, action.payload],
       };
+
+    case "DELETE_FROM_CART":
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (sepet) => sepet._id !== action.payload._id
+        ),
+      };
+
     default:
       return state;
   }
