029bd751412160901f54ca1e79a408a3ac43650e
Elif Kutluğ <elif.kutlugg@hotmail.com>
2023-03-23 18:14:06 +0300

    Ödeme Metodu Başladı

---
+++ client/src/pages/CheckoutPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import StripeCheckout from "react-stripe-checkout";
+import Swal from "sweetalert2";
+import { checkoutOrderAction } from "../actions/orderActions";
+
+function CheckoutPage({ toplamfiyat }) {
+  const dispatch = useDispatch();
+
+  const tokenHandler = (token) => {
+    dispatch(checkoutOrderAction(token, toplamfiyat));
+  };
+
+  return (
+    <div>
+      <StripeCheckout
+        amount={toplamfiyat * 100}
+        shippingAddress
+        billingAddress
+        token={tokenHandler}
+        stripeKey="pk_test_51MV9VIAEY249VO0yq3SGbP130P9341uHf9LRtAZ0DVw27oZ5c1w2pWq83cRBUuzDLDxYUaXGq4i2De2pv5Gnxkrw00m21s3RGJ"
+        currency="TRY"
+      >
+        <button className="btn btn-outline-danger mb-3 w-25">HEMEN ÖDE!</button>
+      </StripeCheckout>
+    </div>
+  );
+}
+
+export default CheckoutPage;
